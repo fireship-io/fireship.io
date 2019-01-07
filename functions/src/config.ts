7c6609c1f46851ac6bc47b227336ea79c7333e3a
codediodeio <hello@jeffdelaney.me>
2019-01-07 16:23:18 -0700

    fix: remove dev app from build options, causing CI error

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,6 +1,3 @@
-export const functionsURL = 'https://us-central1-fireship-dev-17429.cloudfunctions.net'
-
-
 import * as admin from 'firebase-admin';
 export const db = admin.firestore();
 const settings = { timestampsInSnapshots: true };
