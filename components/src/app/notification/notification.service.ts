1eefd4b0e0411f39b9c0311b9f1de551a8a996c6
Dominik Šimoník <dominik.simonik@gmail.com>
2019-01-18 07:44:32 +0100

    missing seconds of countdown notification & reset

--- components/src/app/notification/notification.service.ts
+++ components/src/app/notification/notification.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, ApplicationRef } from '@angular/core';
-import { timer } from 'rxjs';
+import { interval, timer, Subscription } from 'rxjs';
+import { takeUntil, switchMap } from 'rxjs/operators';
 
-const defaults = { title: 'Hey!', text: '', icon: '🔔', className: '', dismissed: false };
+
+const defaults = { title: 'Hey!', text: '', icon: '🔔', className: '', dismissed: false, countdown: 10};
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +12,42 @@ export class NotificationService {
 
   current;
 
+  private countdown;
+  private waiter;
+
   constructor(private app: ApplicationRef) { }
 
   setNotification(data) {
     this.current = { ...defaults, ...data };
     this.app.tick();
+  }
 
-    // 10 sec wait before dismiss
-    timer(10000).subscribe(() => this.dismiss());
+  startCountdown() {
+    this.resetCountdown();
+    const timingSource = interval(1000);
+    const timing = timer(10000);
+    const example = timingSource.pipe(takeUntil(timing));
+    this.countdown = example.subscribe(() => {
+      this.current.countdown -= 1;
+      this.app.tick();
+    });
+    this.waiter = timing.subscribe(() => {
+      this.dismiss();
+    });
+  }
+
+  resetCountdown() {
+    if (this.countdown instanceof Subscription) {
+      this.countdown.unsubscribe();
+    }
+    if (this.waiter instanceof Subscription) {
+      this.waiter.unsubscribe();
+    }
   }
 
   dismiss() {
+    this.resetCountdown();
     this.current.dismissed = true;
     this.app.tick();
   }
-
-
 }
