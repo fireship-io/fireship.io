cc35afa64a2ca620dc4af4fbae805a116f0079cc
codediodeio <hello@fireship.io>
2024-11-28 06:16:58 -0700

    video player updates

--- app/util/player.ts
+++ app/util/player.ts
@@ -8,16 +8,20 @@ export class UniversalPlayer {
     constructor(public video: string | number, public el: HTMLElement, public type: 'youtube' | 'vimeo') {}
 
     private async setupPlayer() {
+
         if (this.type === 'youtube') {
             const YouTubePlayer = (await import('youtube-player')).default;
             const decoded = atob(this.video as string);
+
             this.ytPlayer = YouTubePlayer(this.el);
             this.ytPlayer.cueVideoById(decoded);
         } else {
             const VimeoPlayer = (await import('@vimeo/player')).default;
-            const decoded = parseInt(atob(this.video as string));
-            // const buildId = parseInt(document.head.dataset.build);
-            this.vimeoPlayer = new VimeoPlayer(this.el, { id: decoded });
+
+            const decoded = await this.decoded();
+            const vimeoId = parseInt(decoded)
+
+            this.vimeoPlayer = new VimeoPlayer(this.el, { id: vimeoId });
         }
 
     }
@@ -27,6 +31,18 @@ export class UniversalPlayer {
         const obj = new UniversalPlayer(video, el, type);
         await obj.setupPlayer();
         return obj;
+    }
+
+    async decoded() {
+        const path = window.location.pathname;
+        const s1 = btoa(path);
+        let p1 = (await import('./s2')).default;
+        const p2 = 'fUCB3Iv';
+        const s2 = p1+p2;
+        let withS = atob(this.video as string)
+        let encoded = withS.slice(s1.length, -s2.length);
+        const decoded = atob(encoded)
+        return decoded;
     }
 
     play() {
